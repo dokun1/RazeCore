0303388f5dfbe90d7709bd9fd7c2e504cd9e3041
David Okun <david@okun.io>
2020-04-08 20:04:53 -0500

    Refactored first Networking API to include dependency injection for easier testing

--- Tests/RazeCoreTests/RazeNetworkingTests.swift
+++ Tests/RazeCoreTests/RazeNetworkingTests.swift
@@ -1,25 +1,61 @@
-//
-//  RazeNetworkingTests.swift
-//  RazeCoreTests
-//
-//  Created by David Okun on 4/8/20.
-//
+/// Copyright (c) 2020 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+
 
 import XCTest
 @testable import RazeCore
 
+class NetworkSessionMock: NetworkSession {
+  var data: Data?
+  var error: Error?
+  
+  func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
+    completionHandler(data, error)
+  }
+  
+  
+}
+
 final class RazeNetworkingTests: XCTestCase {
   func testLoadDataCall() {
     let manager = RazeCore.Networking.Manager()
+    let session = NetworkSessionMock()
+    manager.session = session
     let expectation = XCTestExpectation(description: "Called for data")
-    guard let url = URL(string: "https://raywenderlich.com") else {
-      return XCTFail("Could not create URL properly")
-    }
+    let data = Data([0, 1, 0, 1])
+    session.data = data
+    let url = URL(fileURLWithPath: "url")
     manager.loadData(from: url) { result in
       expectation.fulfill()
       switch result {
         case .success(let returnedData):
-          XCTAssertNotNil(returnedData, "Response data is nil")
+          XCTAssertEqual(data, returnedData, "manager returned unexpected data")
         case .failure(let error):
           XCTFail(error?.localizedDescription ?? "error forming error result")
       }
